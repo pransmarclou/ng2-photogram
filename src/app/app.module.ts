da0a0a6720e65002e978dfc3edcde202d8b3b07a
prans <fhranzalimorong@gmail.com>
2019-05-15 18:44:31 +0800

    add: other components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { AllPostsComponent } from './all-posts/all-posts.component';
+import { FollowingComponent } from './following/following.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { MyPostsComponent } from './my-posts/my-posts.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AllPostsComponent,
+    FollowingComponent,
+    FavoritesComponent,
+    MyPostsComponent,
+    LoginComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,

34c5c43ec556335b167a2749f19ac6d204dfb2f6
prans <fhranzalimorong@gmail.com>
2019-05-27 19:24:52 +0800

    update: hide menu content

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  isLoggedIn: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+    firebase.auth().onAuthStateChanged( userData => {
+      if (userData && userData.emailVerified) {
+        this.isLoggedIn = true;
+      } else {
+        this.isLoggedIn = false;
+      }
+    });
   }
-
 }

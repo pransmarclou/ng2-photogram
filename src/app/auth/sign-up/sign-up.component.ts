672525aaa9d8bbdba576ecdd064c2b0b789e8132
prans <fhranzalimorong@gmail.com>
2019-07-20 18:39:42 +0800

    update: fix sign-up issues

--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-sign-up',
@@ -12,4 +14,20 @@ export class SignUpComponent implements OnInit {
   ngOnInit() {
   }
 
+  onSubmit (form: NgForm) {
+
+    const fullname = form.value.fullname;
+    const email = form.value.email;
+    const password = form.value.password;
+
+    //console.log(fullname, email, password);
+
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then(userData => {
+        console.log(userData);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 }
